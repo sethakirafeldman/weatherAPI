4d16a44cc41ede9ced87cdb99954c4db30737fe2
namdlefhtes <namdlefhtes@gmail.com>
2021-11-24 09:28:10 -0500

    error handling and styling

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
+const weatherMain = document.getElementById("weatherMain");
+
 // promise function ver
-const getWeather = (city) => {
-    const location = `${city}`;
-    const apiKey = `3bfc1d6dd8d349f3ecab46491d371c9d`;
-    const units = `&units=metric`
-    const apiLocation = `https://api.openweathermap.org/data/2.5/weather?q=${location}${units}&APPID=${apiKey}`;
-    fetch(apiLocation, {mode: 'cors'} )
-        .then((response) =>  {
-            // console.log(response.json());
-            return response.json();
-        })
-        .then((response) => {
-            console.log(response);
-            weatherDisplay(response);
-        })
-        .catch ((error) => {
-            console.log(`error`);
-        })
-};
+// const getWeather = (city) => {
+//     const location = `${city}`;
+//     const apiKey = `3bfc1d6dd8d349f3ecab46491d371c9d`;
+//     const units = `&units=metric`
+//     const apiLocation = `https://api.openweathermap.org/data/2.5/weather?q=${location}${units}&APPID=${apiKey}`;
+//     fetch(apiLocation, {mode: 'cors'} )
+//         .then((response) =>  {
+//             // console.log(response.json());
+//             return response.json();
+//         })
+//         .then((response) => {
+//             // console.log(response);
+//             weatherDisplay(response);
+//         })
+//         .catch ((error) => {
+//             console.log(`error`);
+
+//         })
+// };
 
 //async/await function ver.
 const getWeatherTwo = async function(city) {
-    const location = `${city}`;
+    const location = `${city}`; // seems redundant
     const apiKey = `3bfc1d6dd8d349f3ecab46491d371c9d`;
     const units = `&units=metric`
     const apiLocation = `https://api.openweathermap.org/data/2.5/forecast?q=${location}${units}&APPID=${apiKey}`;
     // waits for server to grab data before passing into weatherDisplay.
     const response = await fetch(apiLocation, {mode: 'cors'});
     const weathData = await response.json();
-    console.log(weathData);
-    weatherDisplay(weathData);
-};
 
+    if (weathData.cod == "404" || weathData.cod == "400") {
+        weatherMain.innerHTML = "";
+        let errorDisplay = document.createElement("H3");
+        errorDisplay.innerText = `Location not found.`
+        weatherMain.appendChild(errorDisplay);
+    }
+    else if (weathData.cod == "200") {
+        console.log(weathData);
+        weatherDisplay(weathData);        
+    };
+};
+// adds eventlistener to form.
 const cityField = document.getElementById("cityName");
 const submitBtn = document.getElementById("submitBtn");
 document.getElementById("submitBtn").addEventListener("click", () => {
@@ -64,15 +76,23 @@ const weatherDisplay = (resp) => {
         ];
         let div = document.createElement("div");
         let objKey = (Object.keys(resp.list[0].main)[i]);
-        console.log(objKey);
+        // console.log(objKey);
         div.id = objKey;
+        div.classList.add("weatherSquare");
         div.innerHTML = `<h4>${weatherKeys[i]}:</h2> ${resp.list[0].main[objKey]}`;
         weatherM.appendChild(div);
     };
     let description = document.createElement("div");
-    description.innerHTML = `<h4>Conditions:</h4> ${resp.list[0].weather[0].description}`;
+    description.innerHTML = `<h4>Conditions:</h4>`;
     description.id = "conditions";
-    
+    let descText = document.createElement("p");
+    descText.innerText = `${resp.list[0].weather[0].description}`;
+    description.appendChild(descText);
+    description.classList.add("weatherSquare");
+
+    // // hide unnecessary values from DOM.
+    document.getElementById("temp_kf").remove();
+
     // api call to get weather icons
     let iconCode = `${resp.list[0].weather[0].icon}`;
     let iconSRC = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
@@ -80,6 +100,8 @@ const weatherDisplay = (resp) => {
     icon.src = iconSRC;
     description.appendChild(icon);
     weatherM.appendChild(description);
+
+    
 };
 
 // styling options
