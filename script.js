cc6675f045c4208103fdbe5135e859c43ef2638b
namdlefhtes <namdlefhtes@gmail.com>
2021-12-03 10:23:14 -0500

    styling + attribution

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const weatherMain = document.getElementById("weatherMain");
 const tempUnit = "°C";
 const units = `metric`;
 const apiKey = `3bfc1d6dd8d349f3ecab46491d371c9d`;
+const locationCheck = document.getElementById("locationCheck");
 
 const getIcon = (iconCode, appendable)=> {
     let icon = new Image();
@@ -19,16 +20,36 @@ const elMaker = (type, txt, id, append) => {
 };
 
 const getBackground = async (city) => {
-    const unsplashAPI = `https://api.unsplash.com/search/photos?page=1&query=${city}&client_id=YRV9QpR8OK8cKkF0QFk5yOMLv5DeSxQeVMPE1nijnBY`;
+    const randNum = Math.floor(Math.random()*10);
+    const unsplashAPI = `https://api.unsplash.com/search/photos?page=${randNum}&query=${city}&orientation=portrait&client_id=YRV9QpR8OK8cKkF0QFk5yOMLv5DeSxQeVMPE1nijnBY`;
     const photoFetch = await fetch(unsplashAPI,{mode:`cors`});
     const photoData = await photoFetch.json();
-    let photo = photoData.results[0].urls.full;
+    let photo = photoData.results[randNum].urls.full;
 
-    console.log(photoData.results[0].urls);    
+    console.log(photoData.results);    
 
     // apply photo as background
     weatherMain.style.backgroundImage = `url(${photo})`;
     // need to attribute to owner.
+    
+   
+
+    setTimeout(()=> {
+        let imageAuthor = `${photoData.results[randNum].user.username}`;
+        console.log(imageAuthor);
+        let ownerDiv = document.createElement("div");
+        ownerDiv.id = "authorInfo";
+        ownerDiv.classList.add("weatherSquare");
+        let ownerLink = photoData.results[randNum].links.html;
+        ownerDiv.innerHTML = `Image of <strong>${city}</strong> randomly selected from unsplash.com. <br> Author: <strong>${imageAuthor}</strong> 
+        <br><a href="${ownerLink}" target="_blank">${ownerLink}</a>`;
+
+
+        weatherMain.append(ownerDiv);
+    }, 1000);
+    // apend ownerDiv AFTER photo loads to add to end of weatherMain div.
+    // 
+    
 
 };
 
@@ -105,7 +126,7 @@ const weatherDisplay = (resp) => {
     const gridContainer = document.createElement("div");
     gridContainer.id = "grid-container";
     weatherMain.appendChild(gridContainer);
-
+    document.getElementById("city").classList.add("weatherSquare");
     // create grid items for temp, feels like, temp min, temp max, pressure.
     for (let i = 0; i < Object.keys(resp.main).length; i++) {
         let div = document.createElement("div");
@@ -154,7 +175,7 @@ const genHourly = async function(lat, lon) {
         let timeOfDay = dateConvert.toLocaleTimeString().slice(-2);
         dateConvert = dateConvert.toLocaleTimeString().slice(0,-6);
         dateConvert = `${dateConvert} ${timeOfDay}`;
-        hourDiv.innerText = dateConvert;
+        hourDiv.innerHTML = `<strong>${dateConvert}</strong>`;
         hourlyCont.appendChild(hourDiv);
 
         //add hourly weather to each section.
@@ -164,8 +185,8 @@ const genHourly = async function(lat, lon) {
         hourlyWeather.classList.add("hourly-data");
         hourlyWeather.innerHTML =
             `Temperature: ${Math.round(hourlyData.temp)}${tempUnit}<br>
-            Feels Like: ${Math.round(hourlyData["feels_like"])}<br>
-            Conditions: ${hourlyData.weather[0].description}`;
+            Feels Like: ${Math.round(hourlyData["feels_like"])}${tempUnit}<br>
+            ${hourlyData.weather[0].description}`;
         hourDiv.appendChild(hourlyWeather);
         // hourlyWeather.appendChild(icon);    
         getIcon(hourlyData.weather[0].icon, hourlyWeather);
@@ -180,7 +201,7 @@ const getWeekly = async (lat, lon) => {
     let weeklyAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,alerts,hourly&units=${units}&appid=${apiKey}`;
     const weeklyResp = await fetch(weeklyAPI, {mode:`cors`});
     const weeklyRespData = await weeklyResp.json();
-    console.log(weeklyRespData);
+    // console.log(weeklyRespData);
 
     //weekly container
     const weeklyCont = document.createElement("div");
@@ -189,7 +210,7 @@ const getWeekly = async (lat, lon) => {
     weatherMain.appendChild(weeklyCont);
 
     // day divs
-    for (let k = 0; k < 7; k++) {
+    for (let k = 0; k < 6; k++) {
         
         let dayDiv = document.createElement("div");
         dayDiv.id =`day-${k+1}`;
@@ -197,22 +218,22 @@ const getWeekly = async (lat, lon) => {
         let dayData = weeklyRespData.daily[k];
         let dateConvert = new Date(dayData.dt*1000);
         let dateTrim = dateConvert.toDateString().slice(0,-4); // removes year
-        dayDiv.innerHTML = `${dateTrim}`;
+        dayDiv.innerHTML = `<strong>${dateTrim}</strong>`;
         weeklyCont.appendChild(dayDiv);
         // day content
         let dailyData = document.createElement("div");
         dailyData.classList.add("daily-data");
-        dailyData.innerHTML = `
-        Temperature: ${Math.round(dayData.temp.day)}<br>
-        Feels Like: ${Math.round(dayData["feels_like"].day)} <br>
-        Conditions: ${dayData.weather[0].description}`;
-        dayDiv.appendChild(dailyData);
+        dailyData.innerHTML = `${Math.round(dayData.temp.max)}${tempUnit}  |  ${Math.round(dayData.temp.min)}${tempUnit}<br>
+        ${dayData.weather[0].description}`;
         getIcon(dayData.weather[0].icon, dailyData);
+        dayDiv.appendChild(dailyData);
+        
     };
 };
 
 getWeekly(lat, lon);
 getBackground(cityField.value);
+
 cityField.value ="";
 
 };
